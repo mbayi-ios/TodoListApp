5f040526a2069b2952a00328fc443c55037640ea
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-08-26 08:38:17 +0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Amby on 26/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

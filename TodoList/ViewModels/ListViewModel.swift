015764c62cec98b03f4824f2f89622d7029b8183
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-08-26 10:49:21 +0300

    using UserDefaults to save data

--- TodoList/ViewModels/ListViewModel.swift
+++ TodoList/ViewModels/ListViewModel.swift
@@ -8,21 +8,34 @@
 import Foundation
 
 class ListViewModel: ObservableObject {
-    @Published var items: [ItemModel] = []
+    @Published var items: [ItemModel] = [] {
+        didSet {
+            saveItems()
+        }
+    }
+
+    let itemsKey: String = "items_list"
 
     init() {
         getItems()
     }
 
     func getItems() {
-        let newItems =  [
-            ItemModel(title: "This is the first title", isCompleted: false),
-            ItemModel(title: "this is the second title", isCompleted: true),
-            ItemModel(title: "This is the third title", isCompleted: false)
+//        let newItems =  [
+//            ItemModel(title: "This is the first title", isCompleted: false),
+//            ItemModel(title: "this is the second title", isCompleted: true),
+//            ItemModel(title: "This is the third title", isCompleted: false)
+//
+//        ]
+//
+//        items.append(contentsOf: newItems)
 
-        ]
+        guard let data = UserDefaults.standard.data(forKey: itemsKey)
+        ,       let saveditems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else { return }
 
-        items.append(contentsOf: newItems)
+        self.items = saveditems
+
 
     }
 
@@ -48,6 +61,12 @@ class ListViewModel: ObservableObject {
 
         if let index = items.firstIndex(where: { $0.id  == item.id}) {
             items[index] = item.updateCompletion()
+        }
+    }
+
+    func saveItems() {
+        if let enocdedData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(enocdedData, forKey: itemsKey)
         }
     }
 }

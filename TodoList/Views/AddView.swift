21aead827945c3ff81679d1415718de8fb05b92e
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-08-26 10:03:08 +0300

    added alert functionality to the text fied

--- TodoList/Views/AddView.swift
+++ TodoList/Views/AddView.swift
@@ -12,6 +12,9 @@ struct AddView: View {
     @EnvironmentObject var listViewModel: ListViewModel
     @State var textFieldText: String = ""
 
+    @State var alertTitle: String = ""
+    @State var showAlert: Bool = false
+
     var body: some View {
         ScrollView {
             VStack {
@@ -34,11 +37,29 @@ struct AddView: View {
             .padding(14)
         }
         .navigationTitle("Add an Item 🖋")
+        .alert(isPresented: $showAlert, content: getAlert)
     }
 
     func saveButtonPressed() {
-        listViewModel.addItem(title: textFieldText)
-        presentationMode.wrappedValue.dismiss()
+        if textIsAppropriate() {
+            listViewModel.addItem(title: textFieldText)
+            presentationMode.wrappedValue.dismiss()
+        }
+
+    }
+
+    func textIsAppropriate() -> Bool {
+        if textFieldText.count < 3 {
+            alertTitle = "Your new todo item must be at least 3 characters long!😫"
+            showAlert.toggle()
+            return false
+        }
+
+        return true
+    }
+
+    func getAlert() -> Alert {
+        return Alert(title: Text(alertTitle))
     }
 }
 
